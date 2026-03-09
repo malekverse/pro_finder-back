7e827f63dc3e133b415d5be9995fad4fa864ac76
gharrech <gharrech.005@gmail.com>
2026-03-09 18:10:53 +0100

    dash-back V1
    - Implement follow and unfollow company APIs
    - Add CRUD APIs for role management
    - Create dashboard user management APIs
    - Allow companies to assign, update, and remove roles for followers

--- client/src/pages/company/Produits.jsx
+++
@@ -1,9 +0,0 @@
-const Produits = () => {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Produits & Services - En cours de développement</h1>
-    </div>
-  );
-};
-
-export default Produits;
--- client/src/pages/company/Statistiques.jsx
+++
@@ -1,9 +0,0 @@
-const Statistiques = () => {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Statistiques - En cours de développement</h1>
-    </div>
-  );
-};
-
-export default Statistiques;
